6c8f89bbf2fba99671b92cb293d5f84dbe8fc6a2
musmanirfan <usmanmala356@gmail.com>
2024-08-05 16:50:40 -0700

    latest version

--- signup.js
+++ signup.js
@@ -4,6 +4,30 @@ let signpassword = document.querySelector("#password");
 
 
 function signUp() {
+    console.log("started");
     console.log(signName.value, signEmail.value, signpassword.value);
-    
+
+    if (signName === "" && signEmail === "" && signpassword === "") return
+    console.log("working");
+
+    let user = {
+        signName: signName.value,
+        signEmail: signEmail.value,
+        signpassword: signpassword.value
+    };
+    localStorage.setItem("userValues", JSON.stringify(user));
+    localStorage.setItem("login", true);
+    window.location.replace("./index.html");
+
 }
+
+function verifyUser() {
+    let loggedIn = localStorage.getItem("login");
+    if (loggedIn === true) {
+        window.location.replace("./index.html");
+    }
+}
+
+window.addEventListener("load", function () {
+    verifyUser();
+})
