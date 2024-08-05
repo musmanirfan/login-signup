cfd9939fe6d4b1599154d20e0a13d7eb2200b542
musmanirfan <usmanmala356@gmail.com>
2024-08-05 16:28:27 -0700

    latest version

---
+++ login.js
@@ -0,0 +1,7 @@
+let loginEmail = document.querySelector("#email");
+let loginPass = document.querySelector("#password");
+
+function loginFunc () {
+    console.log(loginEmail.value);
+    
+}
---
+++ signup.js
@@ -0,0 +1,9 @@
+let signName = document.querySelector("#text");
+let signEmail = document.querySelector("#email");
+let signpassword = document.querySelector("#password");
+
+
+function signUp() {
+    console.log(signName.value, signEmail.value, signpassword.value);
+    
+}
