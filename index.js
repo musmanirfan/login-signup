f738d35d0b2b41ab8a39fb1237e3f3507b2e1ea0
musmanirfan <usmanmala356@gmail.com>
2024-08-06 08:40:44 -0700

    latest version

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const isLoggedIn = localStorage.getItem("login")
 if (!isLoggedIn){
     window.location.replace("./signup.html")
 }
+
+
+function logoutFunc() {
+    window.location.replace("./login.html")
+}
--- login.js
+++ login.js
@@ -2,6 +2,11 @@ let loginEmail = document.querySelector("#email");
 let loginPass = document.querySelector("#password");
 
 function loginFunc () {
-    console.log(loginEmail.value);
-    
+    const loginData = JSON.parse(localStorage.getItem("userValues"))
+    console.log(loginData.signEmail, loginData.signpassword, loginEmail, loginPass);
+    if (loginEmail.value === loginData.signEmail && loginPass.value === loginData.signpassword){
+        window.location.replace("./index.html")
+    }else{
+        alert("Plz type right email and password");
+    }
 }
